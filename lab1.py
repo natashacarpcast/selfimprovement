7da8ca29dc2f942a0df4d10c0f72e00c2962afbc
natashacarpcast <natashacarpcast@uchicago.edu>
2024-10-15 14:41:38 -0500

    fixed typo

--- lab1.py
+++ lab1.py
@@ -6,5 +6,5 @@ spark = SparkSession \
         .getOrCreate()
 
 #Read CSV file 
-df = spark.read.csv(".../just_text_submissions.csv")
+df = spark.read.csv("../just_text_submissions.csv")
 df.show()
