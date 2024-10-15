95de27aa99d5826379a798d13208f5460d907642
natashacarpcast <natashacarpcast@uchicago.edu>
2024-10-15 15:29:25 -0500

    trying until vectorization

--- lab1.py
+++ lab1.py
@@ -1,4 +1,8 @@
+#Sources
+#https://github.com/apache/spark/blob/master/examples/src/main/python/ml/tf_idf_example.py
+
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import CountVectorizer, IDF, Tokenizer
 
 spark = SparkSession \
         .builder \
@@ -7,4 +11,13 @@ spark = SparkSession \
 
 #Read CSV file 
 df = spark.read.option("header", True).csv("just_text_submissions.csv")
-df.show()
+
+#Tokenize
+tokenizer = Tokenizer(inputCol="cleaned_text", outputCol="tokenized")
+tokenized_df = tokenizer.transform(df)
+
+#Vectorize
+cv = CountVectorizer(inputCol="tokenized", outputCol="vectorized")
+model_cv = cv.fit(tokenized_df)
+vectorized_df = model_cv.transform(tokenized_df)
+vectorized_df.show()
