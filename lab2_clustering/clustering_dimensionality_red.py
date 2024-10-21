29c7740932ee8e4c794dae9e3224b3097a3ef715
natashacarpcast <natashacarpcast@uchicago.edu>
2024-10-20 20:03:20 -0500

    finished first draft of script

--- lab2_clustering/clustering_dimensionality_red.py
+++ lab2_clustering/clustering_dimensionality_red.py
@@ -21,3 +21,74 @@ scores = ['Care_Score', 'Fairness_Score', 'Loyalty_Score',
        'Fairness_Sentiment', 'Loyalty_Sentiment', 'Authority_Sentiment',
        'Sanctity_Sentiment']
 
+#Make sure they're read as floats
+df_features = df.select(*(F.col(c).cast("float").alias(c) for c in scores))
+df_features = df.features.withColumn('features', F.array(*[F.col(c) for c in scores]))\
+                                                    .select('features')
+
+#Try k-means first with PCA dimensionality reduction
+
+#Create dense vector format for PCA and k means
+vectors = df_features.rdd.map(lambda row: Vectors.dense(row.features))
+features = spark.createDataFrame(vectors.map(Row), ["features_unscaled"])
+
+#Standardize as the main scores and the sentiments have different scales
+standardizer = StandardScaler(inputCol="features_unscaled", outputCol="features")
+model = standardizer.fit(features)
+features = model.transform(features) \
+                .select('features')
+
+#Persist in memory for faster execution
+features.persist()
+
+#Perform dimensionality reduction
+pca = PCA(k=2, inputCol="features", outputCol="pcaFeatures")
+model = pca.fit(features)
+pca_results = model.transform(features).select("pcaFeatures")
+pca_features = pca_results.rdd.map(lambda row: Vectors.dense(row.pcaFeatures))
+pca_features = spark.createDataFrame(pca_features.map(Row), ["features"])
+pca_features.persist()
+
+#Perform clustering with k means. First create function
+def clustering_pca(k):
+    pca_kmeans = KMeans(k, seed=2503)
+    pca_model = pca_kmeans.fit(pca_features)
+    pca_pred = pca_model.transform(pca_features)
+    pca_evaluator = ClusteringEvaluator()
+    silhouette = pca_evaluator.evaluate(pca_pred)
+    print("Silhouette with squared euclidean distance = {} when using PCA and {} k clusters".format(str(silhouette), k))
+
+#Run it for different k 
+k_to_try = [2,5,8]  
+for k in k_to_try:
+    clustering_pca(k)
+
+#Unpersist to free memory for following steps
+features.unpersist()
+pca_features.unpersist()
+    
+#Now try using SVD to see which one performs better
+vectors_rdd = df_features.rdd.map(lambda row: row["features"])
+standardizer_rdd = StandardScalerRDD()
+model = standardizer_rdd.fit(vectors_rdd)
+vectors_rdd = model.transform(vectors_rdd)
+mat = RowMatrix(vectors_rdd)
+svd = mat.computeSVD(2, computeU=True)
+U = svd.U
+s = svd.s
+V = svd.V
+U_df = U.rows.map(lambda row: Row(features=Vectors.dense(row.toArray()))) \
+             .toDF()
+U_df.persist()
+
+#Create function to perform k means clustering, now with SVD
+def clustering_svd(k):
+        svd_kmeans = KMeans(k=3, seed=1)
+        svd_model = svd_kmeans.fit(U_df)
+        svd_predictions = svd_model.transform(U_df)
+        svd_evaluator = ClusteringEvaluator()
+        silhouette = svd_evaluator.evaluate(svd_predictions)
+        print("Silhouette with squared euclidean distance = {} when using SVD and {} k clusters".format(str(silhouette), k))
+
+for k in k_to_try:
+    clustering_svd(k)
