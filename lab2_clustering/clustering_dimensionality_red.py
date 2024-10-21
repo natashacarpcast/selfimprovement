aa3709ca4cb8f4b5b20f75d042e89f06b22cfbc9
natashacarpcast <natashacarpcast@uchicago.edu>
2024-10-20 20:09:29 -0500

    fixed typo

--- lab2_clustering/clustering_dimensionality_red.py
+++ lab2_clustering/clustering_dimensionality_red.py
@@ -23,7 +23,7 @@ scores = ['Care_Score', 'Fairness_Score', 'Loyalty_Score',
 
 #Make sure they're read as floats
 df_features = df.select(*(F.col(c).cast("float").alias(c) for c in scores))
-df_features = df.features.withColumn('features', F.array(*[F.col(c) for c in scores]))\
+df_features = df_features.withColumn('features', F.array(*[F.col(c) for c in scores]))\
                                                     .select('features')
 
 #Try k-means first with PCA dimensionality reduction
